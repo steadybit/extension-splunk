4774facf5239fdd8a45f74f6648df6b7d1b9ac3d
Johannes Edmeier <johannes.edmeier@steadybit.com>
2026-03-02 18:56:27 +0100

    chore: update dependencies

--- e2e/mock_splunk_api_test.go
+++ e2e/mock_splunk_api_test.go
@@ -40,7 +40,7 @@ func createMockSplunkApiServer() *mockServer {
 }
 
 func handler[T any](getter func() T) http.Handler {
-	return exthttp.PanicRecovery(exthttp.LogRequest(exthttp.GetterAsHandler(getter)))
+	return exthttp.PanicRecovery(exthttp.LogRequestWithDefaultLogLevel(exthttp.GetterAsHandler(getter), zerolog.DebugLevel))
 }
 
 func (m *mockServer) getDetectors() extdetectors.Response {
