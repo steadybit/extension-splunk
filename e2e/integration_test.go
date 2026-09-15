48bb54478353edd08a1e6ead7e82e35092db3a58
Daniel Reuter <daniel@steadybit.com>
2026-09-15 11:14:41 +0200

    test(e2e): set the required splunk.accessToken and splunk.ingestBaseUrl
    Neither was set, so the chart rendered them empty. That was tolerated until
    required-but-blank parameters became fatal.

--- e2e/integration_test.go
+++ e2e/integration_test.go
@@ -37,6 +37,8 @@ func TestWithMinikube(t *testing.T) {
 		ExtraArgs: func(m *e2e.Minikube) []string {
 			return []string{
 				"--set", fmt.Sprintf("splunk.apiBaseUrl=http://host.minikube.internal:%s", port),
+				"--set", fmt.Sprintf("splunk.ingestBaseUrl=http://host.minikube.internal:%s", port),
+				"--set", "splunk.accessToken=access-token-123",
 				"--set", "logging.level=trace",
 			}
 		},
